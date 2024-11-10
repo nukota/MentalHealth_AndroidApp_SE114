02350fc9b22b1851fd843d733ca7b54fc8c1d4f9
thanh thanh le <thanhthanh.lethi133@gmail.com>
2024-11-10 18:06:03 +0700

    3 questions screen

---
+++ app/src/main/java/com/example/uplift/ui/screens/questions/MhiQuestions.kt
@@ -0,0 +1,220 @@
+package com.example.uplift.ui
+
+import android.graphics.Color.rgb
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.uplift.R
+import com.example.uplift.data.models.Mhianswers
+import androidx.compose.ui.res.painterResource
+import com.example.uplift.data.models.Mhiquestions
+import com.example.uplift.ui.composables.AnswerOption
+import com.example.uplift.ui.composables.NextPreviousBox
+import java.lang.Integer.parseInt
+
+
+@Composable
+fun Mhiquestions(
+    questions: List<Mhiquestions>,
+    answers:  List<Mhianswers>,
+    onAnswerSelected: (Mhianswers) -> Unit,
+    currentQuestionIndex: Int,
+
+    ) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxWidth()
+            .background(color = Color.White)
+    ) {
+        Row(
+
+        ){
+            Column(
+            ){
+                Row() {
+                    Text(
+                        text = "MHI Test",
+                        color = Color(0xff101010),
+                        style = TextStyle(fontSize = 22.sp, fontFamily = FontFamily(Font(R.font.lemonada))),
+                        modifier = Modifier
+                            .padding(start = 20.dp, top = 20.dp)
+                            .height(43.dp)
+                    )
+                }
+                Row() {
+                    Text(
+                        text = "Mental Health Inventory",
+                        color = Color(0xff999999),
+                        fontFamily = FontFamily(Font(R.font.sansitadwashedfont)),
+                        fontSize = 20.sp,
+                        modifier = Modifier
+                            .height(29.dp)
+                            .padding(start = 20.dp)
+                    )
+                }
+            }
+            Column(
+                horizontalAlignment = Alignment.End,
+                verticalArrangement = Arrangement.spacedBy(40.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end=28.dp, top=28.dp)
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.menu),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(28.dp)
+                )
+            }
+        }
+
+
+        Row(
+            horizontalArrangement = Arrangement.Absolute.Right, // Căn giữa các phần tử trong Row theo chiều ngang
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(start = 52.dp, top = 52.dp)
+                .width(286.dp)
+        ) {
+            if (currentQuestionIndex >= 1) {
+                NextPreviousBox(text = "Previous", onClick = { /* Handle Previous */ })
+            }
+
+            Text(
+                text = "${currentQuestionIndex + 1}/${questions.size}", // Display current question number and total
+                color = Color(0xff101010),
+                textAlign = TextAlign.Center,
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    color = Color(0xff101010),
+                    fontFamily = FontFamily(Font(R.font.inter))
+                ),
+                modifier = Modifier
+                    .width(95.dp)
+                    .height(24.dp)
+                    .wrapContentHeight(align = Alignment.CenterVertically)
+            )
+            NextPreviousBox(text = "Next", onClick = { /* Handle Next */ })
+        }
+
+        //questions
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(top = 9.dp)
+                .width(500.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .requiredWidth(width = 284.dp)
+                    .requiredHeight(height = 132.dp)
+                    .clip(shape = RoundedCornerShape(20.dp))
+                    .background(color = Color.White)
+                    .border(
+                        border = BorderStroke(2.dp, Color.Black),
+                        shape = RoundedCornerShape(20.dp)
+                    )
+            )
+            {
+                Text(
+                    text = questions[currentQuestionIndex].question_text,
+                    color = Color(0xff505050),
+                    textAlign = TextAlign.Center,
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        fontFamily = FontFamily(Font(R.font.sansitadwashedfont))
+                    ),
+                    modifier = Modifier
+                        .offset(x = 9.dp, y = 25.dp)
+                        .requiredWidth(width = 265.dp)
+                        .requiredHeight(height = 58.dp)
+                )
+            }
+        }
+        val filteredAnswers =
+            answers.filter { it.question_id == questions[currentQuestionIndex].question_id }
+        //answer
+        Row(
+            horizontalArrangement = Arrangement.Center, // Căn giữa các phần tử trong Row theo chiều ngang
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(top = 30.dp)
+                .width(500.dp)
+                .height(450.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(20.dp),
+                modifier = Modifier.fillMaxHeight()
+            ) {
+                filteredAnswers.forEach { answer ->
+                    val iconResId = when (answer.answer_order) {
+                        1 -> R.drawable.extremely_happy_1
+                        2 -> R.drawable.very_happy_2
+                        3 -> R.drawable.generally_3
+                        4 -> R.drawable.somtimes_4
+                        5->R.drawable.unhappy_5
+                        6->R.drawable.dissatisfied_6
+                        else -> R.drawable.extremely_happy_1
+                    }
+                    AnswerOption(
+                        text = answer.answer_text,
+                        iconResId = iconResId,
+                        onClick = {
+                            onAnswerSelected(answer)
+                        }
+                    )
+                }
+            }
+        }
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp, top = 20.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.back),
+                contentDescription = "back",
+                modifier = Modifier
+                    .size(24.dp)
+            )
+
+            Text(
+                text = "Back",
+                color = Color.Black,
+                fontFamily = FontFamily(Font(R.font.inter)),
+                fontSize = 24.sp,
+                modifier = Modifier
+                    .padding(start = 10.dp)
+            )
+        }
+
+    }
+}
+
+
